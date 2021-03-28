269549ee45b6965d60aa4d57ead24f0435430a95
Daniel Juventude <deljuven@ymail.com>
2021-03-28 17:14:29 -0300

    basic functions for Automata

--- automata/Automata.cpp
+++ automata/Automata.cpp
@@ -1,10 +1,61 @@
 #include "Automata.hpp"
+#include <string.h>
+#include <iostream>
+#include <stdlib.h>
 
-
-Automata::Automata(/* args */)
+Automata::Automata(unsigned int size, unsigned int qtd, double vMax, bool isClosed) : sizeRoad(size),
+                                                                                      qtdRoad(qtd),
+                                                                                      velocityMax(vMax),
+                                                                                      isClosedSystem(isClosed)
 {
+    this->automato = new int[size * qtd];
+    memset(automato, -0x1, size * qtd * sizeof(int));
 }
 
 Automata::~Automata()
 {
+    delete this->automato;
+    std::cout << "VETOR AUTOMATO LIVRE\n";
+}
+
+int Automata::pos(int x, int y)
+{
+    if (x < 0 || x >= qtdRoad || y < 0 || y >= sizeRoad)
+    {
+        std::cout << "ERROR::OUTBOUND::Posicao (" << x << ", " << y << ") fora do automato" << std::endl;
+        this->~Automata();
+        exit(EXIT_FAILURE);
+    }
+    return x * qtdRoad + y;
+}
+
+void Automata::print()
+{
+    std::cout << "=> ";
+    for (size_t i = 0; i < qtdRoad; i++)
+    {
+        for (size_t j = 0; j < sizeRoad; j++)
+        {
+            int cell = getCell(i, j);
+            if (cell == EMPTY_CELL)
+            {
+                std::cout << "-";
+            }
+            else
+            {
+                std::cout << cell;
+            }
+        }
+        std::cout << std::endl;
+    }
+}
+
+int Automata::getCell(int i, int j)
+{
+    return this->automato[pos(i, j)];
+}
+
+void Automata::setCell(int i, int j, int carIndex)
+{
+    this->automato[pos(i, j)] = carIndex;
 }
--- automata/Automata.hpp
+++ automata/Automata.hpp
@@ -1,13 +1,27 @@
 #ifndef AUTOMATA_HPP
 #define AUTOMATA_HPP
 
+#define OUT_BOUND -2
+#define EMPTY_CELL -1
+
 class Automata
 {
 private:
-    /* data */
+    int *automato;
+    unsigned int sizeRoad; // Tamanho linear de uma pista
+    unsigned int qtdRoad;  // quantidade de pistas
+    double velocityMax;
+    bool isClosedSystem;
+
+    int pos(int, int);
+
 public:
-    Automata(/* args */);
+    Automata(unsigned int size, unsigned int qtd, double vMax, bool isClosed);
     ~Automata();
+
+    void print();
+    int getCell(int, int);
+    void setCell(int, int, int);
 };
 
-#endif AUTOMATA_HPP
+#endif
