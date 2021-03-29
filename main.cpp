3aad4ab12d0bdb263a1eba6d0684c45dba107f42
Daniel Juventude <deljuven@ymail.com>
2021-03-29 15:55:24 -0300

    Parametros passados pelo terminal

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "simulacao/Simulacao.hpp"
+#include <iostream>
 #include <stdlib.h>
 #include <ctime>
 
@@ -6,16 +7,19 @@ int main(int argc, const char *argv[])
 {
     srand(time(NULL));
 
-    /**
-     * Primeiro valor é o tamanho da faixa
-     * segundo é a quantidade de faixas
-     * terceiro é a quantidade de carros
-     * quarto é a velocidade máxima
-     **/
-    Simulacao s(70, 1, 10, 6);
+    if (argc != 6) {
+        std::cout << "Deve-se passar 5 parametros inteiros: qtd_passos tamanho_pista qtd_pista qtd_carros vel_max\n";
+        exit(EXIT_FAILURE);
+    }
+    int qtdPassos = atoi(argv[1]);
+    int comprimentoPista = atoi(argv[2]);
+    int qtdPistas = atoi(argv[3]);
+    int qtdCarros = atoi(argv[4]);
+    int velocidadeMax = atoi(argv[5]);
 
-    // Quantidade de passos do automato
-    s.run(40);
+    Simulacao s(comprimentoPista, qtdPistas, qtdCarros, velocidadeMax);
+
+    s.run(qtdPassos);
 
     return 0;
 }
