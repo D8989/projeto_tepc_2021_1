e37167eb610fc5f5d73b988d50dc5d5768a372a4
Daniel Juventude <deljuven@ymail.com>
2021-04-11 22:32:42 -0300

    mudança de parâmetros

--- exec.sh
+++ exec.sh
@@ -1,7 +1,7 @@
 # Projeto de Tópicos Especiais em Programação de Computador
 # author: Daniel Juventude Moreira
 
-./a.out 30 70 2 7 6 1
+./a.out 100 70 2 10 6 1
 #       |  |  | | | | 0-imprime no terminal; 1-cria arquivo
 #       |  |  | | |Velocidade máxima 
 #       |  |  | |Quantidade de carros
