e8eebf0e61790cd31f07b6630abb9d3fd4626742
Daniel Juventude <deljuven@ymail.com>
2021-04-12 22:24:10 -0300

    outra mudança nos valores dos parâmetros de entrada

--- exec.sh
+++ exec.sh
@@ -1,10 +1,10 @@
 # Projeto de Tópicos Especiais em Programação de Computador
 # author: Daniel Juventude Moreira
 
-./a.out 100 70 2 10 6 1
-#       |  |  | | | | 0-imprime no terminal; 1-cria arquivo
-#       |  |  | | |Velocidade máxima 
-#       |  |  | |Quantidade de carros
-#       |  |  |Quantidade de psistas (o 'y' da malha)
-#       |  |Comprimento da pista (o 'x' da malha)
+./a.out 100 90 2 30 5 1
+#       |   |  | |  | | 0-imprime no terminal; 1-cria arquivo
+#       |   |  | |  |Velocidade máxima 
+#       |   |  | |Quantidade de carros
+#       |   |  |Quantidade de psistas (o 'y' da malha)
+#       |   |Comprimento da pista (o 'x' da malha)
 #       |Quantidade de passos
